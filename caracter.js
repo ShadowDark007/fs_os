3a464e892958bc466bfc55bb63e43f08ab02499b
ShadowDark007 <125370176+ShadowDark007@users.noreply.github.com>
2023-02-17 16:30:08 -0500

    Update caracter.js

--- caracter.js
+++ caracter.js
@@ -4,9 +4,13 @@ const os = require ('os');
 
 const Compu = `host : ${os.hostname()}
 
+const totalMemGB = (totalMemBytes / Math.pow(1024, 3)).toFixed(2);
+const totalMemMB = (totalMemBytes / Math.pow(1024, 2)).toFixed(2);
+
 plataForma : ${os.platform()}
 procesador : ${os.cpus()[0].model}
 arquitectura : ${os.arch()}
 MemoriaEnBits : ${os.totalmem()} bytes`;
+memoria ram total: ${totalMemGB} GB (${totalMemMB} MB);
 
 fs.writeFileSync('./data/terce.txt', Compu);
